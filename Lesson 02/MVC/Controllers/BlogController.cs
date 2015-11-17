772ef0427d3aecb5e401548d964041bc83643523
Aghasi Lorsabyan <lorsabyan@gmail.com>
2015-11-17 14:22:07 +0400

    Initial source commit

---
+++ Lesson 02/MVC/Controllers/BlogController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("article-1")]
+        public ActionResult Article1()
+        {
+            return View("Article1");
+        }
+
+        [ActionName("article-2")]
+        public ActionResult Article2()
+        {
+            return View("Article2");
+        }
+    }
+}
