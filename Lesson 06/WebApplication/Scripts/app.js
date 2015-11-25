4fde5a66c361da4a00ec0867cfeaba878f087a49
Aghasi Lorsabyan <aghasi.lorsabyan@mic.am>
2015-11-25 14:15:17 +0400

    Update

--- Lesson 06/WebApplication/Scripts/app.js
+++ Lesson 06/WebApplication/Scripts/app.js
@@ -33,8 +33,39 @@ var TotoMIC = {
         console.debug(jqXHR);
 
         $.each(people, function (index, person) {
-            $("#people").append("<li>" + person.Name + "</li>")
+            $("#people")
+                .append(
+                '<li id="' + person.Id + '">' +
+                person.Name + '<button data-id="' + person.Id + '" type="button" class="btn btn-danger">Remove</button>' +
+                "</li>");
+
+            $("#" + person.Id + " button").click(TotoMIC.removePerson);
         });
+    },
+
+    removePerson: function(event) {
+        event.preventDefault();
+        var id = $(this).data("id");
+
+        var ajaxSettings = {
+            url: TotoMIC.buildUrl("peopleApi", id),
+            type: "DELETE"
+        };
+
+        var deferred = $.ajax(ajaxSettings);
+
+        deferred.done(TotoMIC.removePersonDone);
+        deferred.fail(TotoMIC.removePersonFail);
+    },
+
+    removePersonDone: function (person, textStatus, jqXHR) {
+        $("#" + person.Id).slideUp(500, function () {
+            $(this).remove();
+        });
+    },
+
+    removePersonFail: function (jqXHR, textStatus, errorThrown) {
+
     },
 
     init: function () {
