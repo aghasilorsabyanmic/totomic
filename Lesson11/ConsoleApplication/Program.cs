781047b1c2858213a095144ff437fc0c05906f4d
Aghasi Lorsabyan <aghasi.lorsabyan@mic.am>
2015-11-30 13:57:06 +0400

    Initial

---
+++ Lesson11/ConsoleApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication.ServiceReference1;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoWork();
+            Console.WriteLine("Hello");
+            Console.ReadKey();
+        }
+
+        public static async void DoWork()
+        {
+            Service1Client proxy = new Service1Client();
+            Console.WriteLine(await proxy.GetDataAsync(5));
+            var game = proxy.GetGameById(Guid.NewGuid().ToString());
+            Console.WriteLine($"{game.UserId} - {game.Created}");
+        }
+    }
+}
